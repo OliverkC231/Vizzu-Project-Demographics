64fc58c803c026a9e8509aeb53a1b675f0fdc3d9
OliverkC231 <166536374+OliverkC231@users.noreply.github.com>
2024-07-12 12:10:15 +0200

    Update demographics.py

--- demographics.py
+++ demographics.py
@@ -76,12 +76,11 @@ def get_generation(year):
 
 # Number input for year with automatic generation matching
 selected_year = st.slider('Year Born', min_value=1950, max_value=2024, value=1980)
+df['SelectedYear'] = df['Year'].apply(lambda x: 'yes' if x == selected_year else 'no')
 generation = get_generation(selected_year)
 
 
 if st.button('Create Story'):
-
-    df['SelectedYear'] = df['Year'].apply(lambda x: 'yes' if x == selected_year else 'no')
 
     # Wrap the presentation in a centered div
     st.markdown('<div class="centered">', unsafe_allow_html=True)
