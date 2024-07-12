f25a08ac9ee94197da5bea375ae24a9aca471020
OliverkC231 <166536374+OliverkC231@users.noreply.github.com>
2024-07-12 15:13:18 +0200

    Update demographics.py

--- demographics.py
+++ demographics.py
@@ -370,7 +370,6 @@ if st.button('Create Story'):
                     'y': 'Population',
                     'stackedBy': 'Generation',
                     'color': 'Generation',
-                    'label': 'Population',
                     'title': f"Distribution of all {g_type} born since 1950 worldwide"
                 }
             )
